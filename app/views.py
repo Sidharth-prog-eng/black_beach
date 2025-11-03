81c064b58953f8698d456c2d0195c3e9b263e41b
Sidharth-prog-eng <sidharthsidhu7005@gmail.com>
2025-11-03 05:58:21 +0000

    final design

--- app/views.py
+++ app/views.py
@@ -2,16 +2,21 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'is_homepage': True})
+
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {'is_homepage': False})
+
 def contact(request):
-    return render(request, 'contact.html')
+    return render(request, 'contact.html', {'is_homepage': False})
+
 def rooms(request):
-    return render(request, 'rooms.html')
+    return render(request, 'rooms.html', {'is_homepage': False})
+
 def amenities(request):
-    return render(request, 'amenities.html')
+    return render(request, 'amenities.html', {'is_homepage': False})
+
 def attractions(request):
-    return render(request, 'attractions.html')
+    return render(request, 'attractions.html', {'is_homepage': False})
 
 
